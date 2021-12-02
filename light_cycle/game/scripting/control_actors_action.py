6bfc311b7a2eb867382dc48605e40e3cfa615e0d
Evan Thomson <evanthomson1819@gmail.com>
2021-12-02 16:15:39 -0700

    Added a second cycle movement keys.

--- light_cycle/game/scripting/control_actors_action.py
+++ light_cycle/game/scripting/control_actors_action.py
@@ -21,6 +21,7 @@ class ControlActorsAction(Action):
         """
         self._keyboard_service = keyboard_service
         self._direction = Point(constants.CELL_SIZE, 0)
+        self._direction2 = Point(constants.CELL_SIZE, 0)
 
     def execute(self, cast, script):
         """Executes the control actors action.
@@ -45,7 +46,23 @@ class ControlActorsAction(Action):
         if self._keyboard_service.is_key_down('s'):
             self._direction = Point(0, constants.CELL_SIZE)
         
+        # left
+        if self._keyboard_service.is_key_down('j'):
+            self._direction2 = Point(-constants.CELL_SIZE, 0)
+        
+        # right
+        if self._keyboard_service.is_key_down('l'):
+            self._direction2 = Point(constants.CELL_SIZE, 0)
+        
+        # up
+        if self._keyboard_service.is_key_down('i'):
+            self._direction2 = Point(0, -constants.CELL_SIZE)
+        
+        # down
+        if self._keyboard_service.is_key_down('k'):
+            self._direction2 = Point(0, constants.CELL_SIZE)
+
         cycle1 = cast.get_first_actor("cycles")
         cycle2 = cast.get_last_actor("cycles")
         cycle1.turn_head(self._direction)
-        cycle2.turn_head(self._direction)
+        cycle2.turn_head(self._direction2)
